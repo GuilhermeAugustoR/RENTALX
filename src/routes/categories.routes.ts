24cf06cb603e71fc5d211a2fa5f43ad20d488cf0
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-03-21 11:17:20 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (req, res) => {
+    const { name, description } = req.body;
+
+    const categoryAlredyExist = categoriesRepository.findByName(name);
+
+    if (categoryAlredyExist) {
+        return res.status(400).json({ error: "Category Alredy Exist!" })
+    }
+
+    categoriesRepository.create({ name, description })
+
+    return res.status(201).send()
+});
+
+categoriesRoutes.get("/", (req, res) => {
+    const all = categoriesRepository.list();
+    return res.json(all)
+})
+
+export { categoriesRoutes }
