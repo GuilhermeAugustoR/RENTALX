4fad5b115bfd0534e8a7427eed5002c3fe5ce916
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-03-22 23:31:57 -0300

    Continuando a implementacao da api

---
+++ src/modules/cars/service/CreateSpecificationService.ts
@@ -0,0 +1,22 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlredyExist = this.specificationRepository.findByName(name);
+
+    if(specificationAlredyExist){
+        throw new Error('Specification alredy exist!')
+    }
+    
+    this.specificationRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationService };
